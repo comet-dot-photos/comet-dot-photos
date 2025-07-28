acf291ec6ea2b9dfffa4500127cdbaf00f8cdb33
DJ Kurlander <dkurlander@yahoo.com>
2025-07-28 11:37:26 -0700

    Removing version from most filenames. Various renames to make filename intent clearer.

---
+++ client/cometInfo.js
@@ -0,0 +1,277 @@
+import * as THREE from './node_modules/three/build/three.module.js'; //should be good
+import {DecalGeometry} from './node_modules/three/examples/jsm/geometries/DecalGeometry.js';
+
+export class CometInfo {
+    static FOV = 2.20746; //Based on campt results - 2.21 in .IMG header
+    static defaultRes = 2048;
+    static MAX_COMET_WIDTH = 4.0;   // Comet does not extend beyond this distance from origin
+    static EXTRA_CLIP_FAR = 400;      // this will mostly avoid clipping if we zoom out w/ trackball control, since far is not updated
+    static map;     // We choose to make the map a class var because we keep the last view's map until the new one's loaded
+    static decal;   // ... and same for decal
+    static lastRequestedImg;  // name of the last requested image, so previous requests don't get displayed!
+    static blankTexture = new THREE.Texture();
+    static mgr = new THREE.LoadingManager();
+    static {
+        this.mgr.onProgress = (str, num, total) => {
+            console.log("In LoadingManager.onProgress: Loading %s: %i/%i", str, num, total);
+        }
+    }
+    static urlPrefix = "";
+    constructor(photoDict) {
+        this.line = null;
+        this.sc_position = new THREE.Vector3();
+        this.sc_position.fromArray(photoDict.sc);
+        this.minDistAlongNormal = photoDict.d1 ?? 100;  // During preprocessing, we don't know d1, so it to an arbitrary value (necessary for computeViewRect).
+        this.maxDistAlongNormal = photoDict.d2;
+        this.distToPlane = this.minDistAlongNormal; // Have this over above nearest point - formerly (this.minDistAlongNormal + this.maxDistAlongNormal) / 2.0;
+        this.ogIndex = photoDict.ogIndex;
+        this.imageRes = ('rz' in photoDict) ? photoDict.rz : CometInfo.defaultRes;
+   
+        this.normal = new THREE.Vector3(...photoDict.cv);
+        this.up = new THREE.Vector3(...photoDict.up);
+        this.computeViewRect();
+ 
+        this.tag = photoDict.nm;
+        this.jpgPath = CometInfo.urlPrefix + 'J80/' + photoDict.nm.substring(1, 7) + '/' + photoDict.nm + '.jpg'
+ 
+        this.fileName = photoDict.nm;
+        this.time = photoDict.ti;
+        //set random image plane it will change soon!
+        this.image_plane = new THREE.Plane()
+        this.image_plane.setFromNormalAndCoplanarPoint(this.normal, (this.sc_position.clone().add(this.normal.clone().multiplyScalar(30))));
+    }
+
+    computeViewRect () {
+        this.planeCenter = this.sc_position.clone().add(this.normal.clone().setLength(this.distToPlane));
+        this.imageWidth = Math.tan(Math.PI*CometInfo.FOV/180.0) * this.distToPlane;
+        this.halfWidth = this.imageWidth/2.0;
+        const midTopLineVec = this.up.clone().setLength(this.halfWidth);
+        const midRightLineVec = midTopLineVec.clone().applyAxisAngle(this.normal, Math.PI/2.0);
+        this.corners = [];
+        this.corners.push(this.planeCenter.clone().add(midTopLineVec).sub(midRightLineVec));    // upper left
+        this.corners.push(this.corners[0].clone().add(midRightLineVec).add(midRightLineVec));   // upper right
+        this.corners.push(this.corners[1].clone().sub(midTopLineVec).sub(midTopLineVec));       // lower right
+        this.corners.push(this.corners[2].clone().sub(midRightLineVec).sub(midRightLineVec));   // lower left
+    }
+
+    createViewRect() {
+        const geom = new THREE.BufferGeometry();
+        const vertices = new Float32Array( [
+            this.corners[0].x, this.corners[0].y, this.corners[0].z,
+            this.corners[1].x, this.corners[1].y, this.corners[1].z,
+            this.corners[2].x, this.corners[2].y, this.corners[2].z,
+            this.corners[3].x, this.corners[3].y, this.corners[3].z,
+        ]);
+        geom.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+        const index = new Uint16Array( [0, 3, 1,   1, 3, 2]);  // two CCW triangles
+        geom.setIndex(new THREE.BufferAttribute(index, 1));
+        this.viewRect = new THREE.Mesh(geom, new THREE.MeshBasicMaterial());
+    }
+
+    loadImage(onLoadFunc) {
+        CometInfo.lastRequestedImg = this.name;
+        const loader = new THREE.TextureLoader(CometInfo.mgr, function(texture) { console.log('Load manager loaded texture: %s', texture.filename);});  // this line is needed, but loadManager argument only necessary for callback
+        const startTime = performance.now();
+        loader.load(this.jpgPath, onLoadFunc);
+
+        CometInfo.mgr.onLoad = function () {
+            const endTime = performance.now();
+            console.log('Texture loading time: %f ms', endTime-startTime);
+        }
+    }
+
+    applyToCamera(camera, orbControls, aspect = 0) {
+        camera.fov = CometInfo.FOV;
+        if (aspect == 0) aspect = window.innerWidth / window.innerHeight;
+        camera.aspect = aspect;
+        camera.near = .1;
+        const scToComet = this.sc_position.clone().length();
+        camera.far = scToComet + CometInfo.MAX_COMET_WIDTH + CometInfo.EXTRA_CLIP_FAR; // EXTRA_CLIP_FAR allows us to zoom out 
+        camera.position.set(this.sc_position.x, this.sc_position.y, this.sc_position.z);
+        camera.up.set(this.up.x, this.up.y, this.up.z);
+        camera.lookAt(this.planeCenter.x, this.planeCenter.y, this.planeCenter.z);
+        camera.updateProjectionMatrix();
+        camera.updateWorldMatrix(true, false);
+
+        if (orbControls) {
+            orbControls.target = this.planeCenter.clone();
+            orbControls.update();
+        }
+    }
+
+    setMinDistAlongNormal(x) {
+        this.minDistAlongNormal = x;
+    }
+    setMaxDistAlongNormal(x) {
+        this.maxDistAlongNormal = x;
+    }
+    setUp(x) {
+        this.up = x.clone();
+    }
+    addOutline(scene) {
+        const lineMaterial = new THREE.LineBasicMaterial({color: 0x0000ff});
+        const linepts = [...this.corners]; // copy of the corners array
+        linepts.push(this.corners[0]);     // close the square
+        const squareGeometry = new THREE.BufferGeometry().setFromPoints(linepts);
+        this.line = new THREE.Line(squareGeometry, lineMaterial);
+        scene.add(this.line);
+    }
+    removeOutline(scene) {
+        if (this.line) {
+            scene.remove(this.line);
+            this.line.material.dispose();
+            this.line.geometry.dispose();
+            this.line = null;
+        }
+    }
+    addDecal(scene, mesh) {
+        this.imageFresh = false;
+        let view = this;
+        function onDecalLoaded(texture) {
+            if (CometInfo.lastRequestedImg != view.name) {
+                texture.dispose();
+                return;
+            } 
+
+            let oldMap = CometInfo.map;
+            let oldDecal = CometInfo.decal;
+            CometInfo.map = texture;
+            const decalMaterial = new THREE.MeshPhongMaterial({
+                map: CometInfo.map,
+                transparent: true,  // what do the scientists want?
+                depthTest: true,
+                depthWrite: false,
+                polygonOffset: true,
+                polygonOffsetFactor: - 4
+            });
+            const euler = new THREE.Euler();
+            const orientation = new THREE.Matrix4();
+            view.imageDepthAlongNormal = view.maxDistAlongNormal - view.minDistAlongNormal;
+            const lookAt = view.sc_position.clone().add((view.normal.clone().multiplyScalar(view.minDistAlongNormal+(view.imageDepthAlongNormal/2))));
+            orientation.lookAt(view.sc_position, lookAt, view.up);
+            euler.setFromRotationMatrix(orientation);
+            const size = new THREE.Vector3(view.imageWidth, view.imageWidth, view.imageDepthAlongNormal+0.05);
+            const decalGeometry = new DecalGeometry(mesh, lookAt, euler, size);
+            CometInfo.decal = new THREE.Mesh(decalGeometry, decalMaterial); //should this be a const??
+            scene.add(CometInfo.decal);
+            view.imageFresh = true;
+            // Cleanup
+            if (oldDecal) {
+                scene.remove(oldDecal);
+                oldDecal.geometry.dispose();
+                oldDecal.material.dispose();
+            }
+            if (oldMap) oldMap.dispose();
+        }
+        this.loadImage(onDecalLoaded);        // now we do it on demand
+    }
+
+    removeDecal(scene) {
+        if (CometInfo.decal) {
+            const oldDecal = CometInfo.decal;
+            scene.remove(oldDecal);
+            oldDecal.geometry.dispose();
+            oldDecal.material.dispose();
+            CometInfo.decal = null;
+        }
+     }
+
+    addProjection (mesh, material) {
+        this.imageFresh = false;
+        let view = this;
+        function onProjectionLoaded(texture) {
+            if (CometInfo.lastRequestedImg != view.name) {
+                texture.dispose();
+                return;
+            } 
+
+            let oldMap = CometInfo.map;
+            CometInfo.map = texture;
+            material.texture = texture;
+            material.camera = new THREE.PerspectiveCamera();
+            view.applyToCamera(material.camera, null, 1.0); // clone the current camera, but set viewing properties for image
+            material.project(mesh);
+            material.needsUpdate = true;
+            material.texture.needsUpdate = true;
+            view.imageFresh = true;
+
+            // cleanup for oldMap
+            if (oldMap) oldMap.dispose();
+        }
+        this.loadImage(onProjectionLoaded);        // now we do it on demand
+    }
+
+    removeProjection (material) {
+        if (CometInfo.map) {
+            /*
+            CometInfo.map.dispose();
+            CometInfo.map = null;
+            */
+            material.texture = CometInfo.blankTexture;
+            material.texture.isTextureProjected = false;
+            material.needsUpdate = true;
+        }
+      }
+
+      LoadImageForOverlay(overlayCanvas) {
+        let view = this;
+        this.imageFresh = false;
+        function onOverlayImageLoaded(texture) {
+            if (CometInfo.lastRequestedImg != view.name) {
+                texture.dispose();
+                return;
+            }
+
+            const oldMap = CometInfo.map;
+            CometInfo.map = texture;
+            view.imageFresh = true;
+
+            // cleanup for oldMap
+            if (oldMap) oldMap.dispose();
+        }
+        this.loadImage(onOverlayImageLoaded);        // now we do it on demand
+    }
+
+    /*
+    removeImageForOverlay() {
+        if (CometInfo.map) {
+            CometInfo.map.dispose();
+            CometInfo.map = null;
+        }
+    }
+    */
+
+    removeSelf(scene){
+        this.removeOutline(scene);
+    }
+
+    saveExtentInfo(bbox, normDepth) {
+        this.bbox = bbox;
+        this.normDepth = normDepth;
+    }
+}
+
+const LARGENUMBER = 1.0e12;     // good enough for our scenes
+
+export class NormalDepth {
+    depthMin;
+    depthMax;
+
+    constructor(min = LARGENUMBER, max = -LARGENUMBER) {
+        this.depthMin = min;
+        this.depthMax = max;
+    }
+
+    expandByVector(vect, norm) {        // normal must be pre-normalized
+        const dotProd = vect.dot(norm);
+        if (dotProd < this.depthMin) this.depthMin = dotProd;
+        if (dotProd > this.depthMax) this.depthMax = dotProd;
+    }
+
+    getDepth() {
+        return (this.depthMax - this.depthMin);
+    }
+}
+
+
+
