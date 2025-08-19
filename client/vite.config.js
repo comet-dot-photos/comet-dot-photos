6da2024082f5c249244d72a1e5542fef1dfaa537
djkurlander <85320037+djkurlander@users.noreply.github.com>
2025-08-19 09:32:05 -0700

    Allow larger transmit chunks in vite compilation.

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig(({ mode, command }) => {
   const isDev = mode === 'development';
   const isBuild = command === 'build'; // 'build' or 'serve'
+  const isDevBuild = isDev && isBuild;
 
   return {
     root: 'src',
@@ -14,7 +15,8 @@ export default defineConfig(({ mode, command }) => {
       emptyOutDir: true,
       assetsInlineLimit: 0,
       sourcemap: isDev ? 'inline' : 'hidden', // examples: dev maps inline, prod hidden
-      minify: isBuild ? 'esbuild' : false
+      minify: isBuild ? 'esbuild' : false,
+      chunkSizeWarningLimit: isDevBuild ? 5000 : 1000,
     }
   };
 });
